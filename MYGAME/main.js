554ee0056ddae0b191b0de14fa61b9aab31770ec
mask <mahsaaaaa@gmail.com>
2021-11-03 11:23:27 +0100

    finish Player1

--- MYGAME/main.js
+++ MYGAME/main.js
@@ -1,7 +1,5 @@
-class cell{
-  
-}
-
+//require("./dataOfCells");
+import { tunnels } from "./dataOfCells.js";
 class player {
   constructor(col, row, ctx, imgStr) {
     var img = new Image();
@@ -20,18 +18,30 @@ class player {
   }
   draw() {
     this.ctx.drawImage(this.img, this.col, this.row, 30, 30);
-    console.log("draw:row", this.row, "col:", this.col);
-    if (this.col<50 && this.row<50)
-    {
-      alert("du hast gewonnen")
-    }
   }
   canMove(chance) {
     console.log("row", this.row, "col:", this.col, chance);
     if (this.row < 50 && this.col - 50 * chance < 0) {
-      //alert("cant move");
       return false;
     } else return true;
+  }
+
+  moveBack() {
+    this.ctx.clearRect(this.col - 5, this.row - 5, 45, 45);
+    if (this.direction == "L") {
+      if (this.col + 50 < 500) this.col = this.col + 50;
+      else if (this.row + 50 > 0) {
+        this.row = this.row + 50;
+        this.direction = "R";
+      }
+    } else {
+      if (this.col - 50 > 0) this.col = this.col - 50;
+      else if (this.row + 50 > 0) {
+        this.row = this.row + 50;
+        this.direction = "L";
+      }
+    }
+    this.draw();
   }
 
   move() {
@@ -51,20 +61,33 @@ class player {
     }
     this.draw();
   }
-  checkWin(){
-    if (this.col<50 && this.row<50)
-    {
-      return true
+  checkWin() {
+    if (this.col < 50 && this.row < 50) {
+      return true;
+    }
+  }
+  moveTunel() {
+    let x = Math.ceil(this.col / 50);
+    let y = 11 - Math.ceil(this.row / 50);
+    const steps = tunnels[y + "," + x] || 0;
+    for (let i = 1; i <= Math.abs(steps); i++) {
+      if (steps > 0) {
+        this.move();
+      } else {
+        this.moveBack();
+      }
     }
   }
 }
 
 var drawGrid = function (w, h, ctx) {
   var ctx;
+  var img = document.getElementById("snakeundladder");
   ctx.canvas.width = w;
   ctx.canvas.height = h;
-  for (x = 0; x <= w; x += 50) {
-    for (y = 0; y <= h; y += 50) {
+  ctx.drawImage(img, 0, 0, w, h);
+  for (let x = 0; x <= w; x += 50) {
+    for (let y = 0; y <= h; y += 50) {
       ctx.moveTo(x, 0);
       ctx.lineTo(x, h);
       ctx.stroke();
@@ -83,8 +106,8 @@ class dice {
     this.img = img;
     document.body.appendChild(img);
     img.onload = () => {
-      this.img = img;
-      this.drawDice();
+    this.img = img;
+    this.drawDice();
     };
   }
   setDice() {
@@ -96,22 +119,28 @@ class dice {
   }
 }
 // run codes when document loaded
-window.addEventListener("DOMContentLoaded", () => {
+  window.addEventListener("DOMContentLoaded", () => {
   const ctx = document.getElementById("myCanvas").getContext("2d");
   drawGrid(500, 500, ctx);
   let player1 = new player(8, 459, ctx, "playerBlue.png");
   document.getElementById("btn").addEventListener("click", () => {
     const dice1 = new dice();
-    MoveSteps = dice1.setDice();
+    let MoveSteps = dice1.setDice();
+    //*************/
+    //let MoveSteps = document.getElementById("INPUT").value;
+    //*********** */
     document.getElementById("lbld").innerHTML = MoveSteps;
-    if (player1.canMove(MoveSteps))
+    if (player1.canMove(MoveSteps)) {
       for (let i = 1; i <= MoveSteps; i++) {
         player1.move();
       }
-     if (player1.checkWin())
-     {
-       alert("*******DU HAST GEWONNEN*******")
-     }
+    }
+    player1.moveTunel();
+    drawGrid(500, 500, ctx);
+    player1.draw();
+    if (player1.checkWin()) {
+      alert("player1*******DU HAST GEWONNEN*******");
+    }
   });
-
+  document.getElementById("btn2").addEventListener("click", () => {});
 });
